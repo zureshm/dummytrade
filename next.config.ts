96998b20ff0a15062577a038fe3d6c0e5851461f
zureshm <zuresh.m@gmail.com>
2026-06-11 19:21:53 +0530

    fuck

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* config options here */
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
